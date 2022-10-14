3b860f40cdda908d25f2356966c7c9ec31c79522
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2022-10-14 23:55:26 +0200

    chore: Refactor general simulator

--- src/general_simulator.ts
+++ src/general_simulator.ts
@@ -17,52 +17,63 @@ import {
  * @param setupOptions Setup options.
  * @param randomNumberProvider Random number provider.
  */
-export const generalSimulator = (
+export function generalSimulator(
   setupOptions: SetupOptions,
   randomNumberProvider: RandomNumberProvider
-): GameSimulator => {
+): GameSimulator {
+  /*eslint complexity: ["error", 5]*/
+  async function pickRandomIndexes(
+    numIndexes: number,
+    excludedIndexes: number[]
+  ): Promise<number[]> {
+    let index: number;
+
+    const indexes = new Array<number>(numIndexes);
+    try {
+      for (let i = 0; i < numIndexes; i += 1) {
+        // The exact number of needed RNG calls is nondeterministic.
+        do {
+          index = await randomNumberProvider.random(0, setupOptions.size - 1);
+        } while (excludedIndexes.indexOf(index) !== -1);
+        indexes[i] = index;
+      }
+    } catch (ex) {
+      throw new Error(
+        `Cannot generate random number. ${
+          ex instanceof Error ? ex.message : ex
+        }`
+      );
+    }
+
+    return indexes;
+  }
+
   /**
    * Runs simulation.
    * @function simulateGame
    * @returns A new GameSummary.
    * @throws {Error} On simulation failure due to random number provider failure.
    */
-  const simulateGame = async (): Promise<GameSummary> => {
-    let error = false;
-
+  async function simulateGame(): Promise<GameSummary> {
     // Picking winning and player indices are independent events.
     const wiPromise = pickRandomIndexes(1, []);
     const piPromise = pickRandomIndexes(1, []);
+    const [winningIndexArray, playerInitialPickedIndexArray] =
+      await Promise.all([wiPromise, piPromise]);
 
-    let winningIndex = -1;
-    try {
-      winningIndex = (await wiPromise)[0];
-    } catch (ex) {
-      error = true;
-    }
-
-    let playerInitialPickedIndex = -1;
-    try {
-      playerInitialPickedIndex = (await piPromise)[0];
-    } catch (ex) {
-      error = true;
-    }
+    const [winningIndex] = winningIndexArray;
+    const [playerInitialPickedIndex] = playerInitialPickedIndexArray;
 
     const indexesExcludedFromReveal = Array.from(
       new Set([winningIndex, playerInitialPickedIndex])
     );
 
-    let revealedLosingIndexes: number[] = [];
-    try {
-      revealedLosingIndexes = await pickRandomIndexes(
-        setupOptions.size - 2,
-        indexesExcludedFromReveal
-      );
-    } catch (ex) {
-      error = true;
-    }
+    let revealedLosingIndexes: number[] = await pickRandomIndexes(
+      setupOptions.size - 2,
+      indexesExcludedFromReveal
+    );
 
-    let confirmedPlayerPickedIndex = -1;
+    let confirmedPlayerPickedIndex;
     if (setupOptions.isPlayerStubborn) {
       confirmedPlayerPickedIndex = playerInitialPickedIndex;
     } else {
@@ -70,17 +81,10 @@ export const generalSimulator = (
         playerInitialPickedIndex,
         ...revealedLosingIndexes,
       ];
-      try {
-        confirmedPlayerPickedIndex = (
-          await pickRandomIndexes(1, excludedIndexes)
-        )[0];
-      } catch (ex) {
-        error = true;
-      }
-    }
-
-    if (error) {
-      throw new Error("Simulation failed.");
+      [confirmedPlayerPickedIndex] = await pickRandomIndexes(
+        1,
+        excludedIndexes
+      );
     }
 
     return {
@@ -91,35 +95,9 @@ export const generalSimulator = (
       setupSize: setupOptions.size,
       winningIndex,
     };
-  };
-
-  const pickRandomIndexes = async (
-    numIndexes: number,
-    excludedIndexes: number[]
-  ): Promise<number[]> => {
-    let index: number;
-
-    const indexes = new Array<number>(numIndexes);
-    for (let i = 0; i < numIndexes; i += 1) {
-      do {
-        // The exact number of needed RNG calls is nondeterministic.
-        try {
-          index = await randomNumberProvider.random(0, setupOptions.size - 1);
-        } catch (ex) {
-          throw new Error(
-            `Cannot generate random number. ${
-              ex instanceof Error ? ex.message : ex
-            }`
-          );
-        }
-      } while (excludedIndexes.indexOf(index) !== -1);
-      indexes[i] = index;
-    }
-
-    return indexes;
-  };
+  }
 
   return {
     simulateGame,
   };
-};
+}
