89a65753694ebf32884ec7aa8044d54cb2646dde
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2022-10-29 18:26:17 +0200

    ci: Add basic ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+  },
+};
