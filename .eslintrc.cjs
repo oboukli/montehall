713022cad0f85476ebd5de89f174bfd2499096c5
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-03-04 18:03:08 +0100

    build: Add eslint-plugin-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,27 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json", "./tsconfig.spec.json"],
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  rules: {
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".mts", ".ts", ".d.ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        project: ["tsconfig.json", "tsconfig.spec.json"],
+      },
+    },
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:prettier/recommended",
     "prettier",
   ],
